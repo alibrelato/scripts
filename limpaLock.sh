2f94841b2e8d5d0a773c714fe5c7c1300a66e2a2
alibrelato <alibrelato@gmail.com>
2016-05-24 09:36:39 -0300

    Update limpaLock.sh

--- limpaLock.sh
+++ limpaLock.sh
@@ -11,43 +11,13 @@
 
 ### VARIAVEIS DE AMBIENTE ###
 
-### verificar esses arquivos para o WWW ###
-#/usr/local/apache/htdocs/portal/shellscript/exec_Daemon_BotSB.ctl
-#
-#/usr/local/apache/htdocs/portal/shellscript/exec_Exportador_BotSB.sh
-#/usr/local/apache/htdocs/portal/shellscript/exec_Exportador_BotSB.ctl
-#
-#/usr/local/apache/htdocs/portal/shellscript/exec_Clean_process_BotSB.sh
-#/usr/local/apache/htdocs/portal/shellscript/exec_Clean_process_BotSB.ctl
-#
-#/usr/local/apache/htdocs/portal/shellscript/limpar_atvdvarj/limpar_atvdvarj.sh
-#/usr/local/apache/htdocs/portal/shellscript/limpar_atvdvarj/limpar_atvdvarj.ctl
-###
-### NEWWDB
-# /products/scripts/ativo.ctl
-
-
 hostname=`hostname`
 log=/tmp/limpaLock.log
 data=`date "+%d/%m/%Y %H:%M:%S"`
-email="alphatec@shoppingbrasil.com.br"
+email="email@dominio"
 listaArquivos=( `echo "
-/home/redimagem/imgs_digitalizacao/controle.txt
-/home/interconect/imgs_digitalizacao/controle.txt
-/data0/sb_digitalizacao/imgs_digitalizacao/controle.txt
-/home/argentina/imgs_digitalizacao/controle.txt
-/home/belohorizonte/imgs_digitalizacao/controle.txt
-/home/brasilia/imgs_digitalizacao/controle.tx
-/home/goiania/imgs_digitalizacao/controle.txt
-/home/manaus/imgs_digitalizacao/controle.txt
-/home/recife/imgs_digitalizacao/controle.txt
-/home/riodejaneiro/imgs_digitalizacao/controle.txt
-/home/monitora1/imgs_digitalizacao/controle.txt
-/home/monitora2/imgs_digitalizacao/controle.txt
-/home/monitora3/imgs_digitalizacao/controle.txt
-/home/monitora-cdmp/imgs_digitalizacao/controle.txt
-/data_media/ftp/home/centrodigi016/imgs_digitalizacao/controle.txt
-/usr/local/apache/htdocs/portal/modules/integracoes/clientes/administracao_sb/ativo.ctl
+/home/controle.txt
+/home/ativo.ctl
 "` )
 
 ### INICIO ###
